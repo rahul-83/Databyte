9d29b33638bfcc5242af0f2cc97d96668ca14c9a
rahul-83 <63275013+rahul-83@users.noreply.github.com>
2021-01-02 14:49:13 +0530

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,10 @@
+n = 100
+for i in range(n):
+    if ((i + 1) % 15 == 0):
+        print("FizzBuzz")
+    elif((i+1)%3==0):
+        print("Fizz")
+    elif ((i + 1) % 5 == 0):
+        print("Buzz")
+    else:
+        print(i+1)
